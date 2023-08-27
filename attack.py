8ee49604f87494f18484f1a0d49e6f296a3f4c1c
Anshik Sahu <97736240+AnshikSahu@users.noreply.github.com>
2023-08-27 14:45:20 +0530

    Add files via upload

---
+++ attack.py
@@ -0,0 +1,15 @@
+from encrypt import encrypt
+
+"""
+You can implement helper function here if you want
+"""
+
+def attack():
+    """
+    TODO: Implement your code here, You can use encrypt or decrypt or both function for your attack
+    """
+
+    """
+    Return the secret
+    """
+    return ""
